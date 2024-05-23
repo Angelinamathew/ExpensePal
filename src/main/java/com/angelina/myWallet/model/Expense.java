55657c5e53e33b9ad59469fb3da35f82abccba26
Angelinamathew <angelinamathew707@gmail.com>
2024-05-23 12:36:21 -0400

    added expense

---
+++ src/main/java/com/angelina/myWallet/model/Expense.java
@@ -0,0 +1,20 @@
+package com.angelina.myWallet.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "expense")
+public class Expense {
+
+    @Id
+    private Long id;
+    private
+}
