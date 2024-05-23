8950110aebbab65a9b903f1aa37163567009a974
Angelinamathew <angelinamathew707@gmail.com>
2024-05-23 12:46:41 -0400

    added expense relationship

--- src/main/java/com/angelina/myWallet/model/Expense.java
+++ src/main/java/com/angelina/myWallet/model/Expense.java
@@ -2,10 +2,13 @@ package com.angelina.myWallet.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,5 +19,18 @@ public class Expense {
 
     @Id
     private Long id;
-    private
+    //pin the time for timestamp
+    private Instant expenseDate;
+
+    private String description;
+
+    //many expenses can go under one category
+    @ManyToOne
+    private Category category;
+
+    //Many expense goes to one user
+    @ManyToOne
+    private User user;
+
+
 }
