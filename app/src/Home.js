85cf20537c36bf4c91df294d506eae1e4fd8729b
Angelina Mathew <angelinamathew@Angelinas-MacBook-Pro.local>
2024-06-04 17:01:15 -0400

    styling pages

--- app/src/Home.js
+++ app/src/Home.js
@@ -4,24 +4,24 @@ import AppNav from './AppNav';
 class Home extends Component {
     state = {}
 
-    render() { 
+    render() {
         const mystyle = {
             color: "black",
             padding: "10px",
             fontFamily: "Times Roman"
-          };
+        };
         return (
             <div>
-             <AppNav/>
-             <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'center' }}>
-               <img src="/home1.jpg" alt="App Logo" style={{ width: '800px', height: 'auto' }} />
-             </div>
-             <h1 style={mystyle}>
-                Welcome to my Expense Pal app
-             </h1>
+                <AppNav />
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h1 style={mystyle}>
+                        Welcome to my Expense Pal app
+                    </h1>
+                    <img src="/home1.jpg" alt="App Logo" style={{ width: '800px', height: 'auto', justifyContent: "right" }} />
+                </div>
             </div>
         );
     }
 }
- 
+
 export default Home;
